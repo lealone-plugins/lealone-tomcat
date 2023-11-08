5fba42cfc55b0e32c4dae1c1ad4e2dc80bc5bb5a
Honghua Zhu <zhh200910@gmail.com>
2023-11-09 07:47:09 +0800

    更改测试类所在的包名

--- src/test/java/org/lealone/plugins/tomcat/test/TomcatServerStart.java
+++ src/test/java/org/lealone/plugins/tomcat/test/TomcatServerStart.java
@@ -3,7 +3,7 @@
  * Licensed under the Server Side Public License, v 1.
  * Initial Developer: zhh
  */
-package org.lealone.plugins.redis.test;
+package org.lealone.plugins.tomcat.test;
 
 import org.lealone.main.Lealone;
 
