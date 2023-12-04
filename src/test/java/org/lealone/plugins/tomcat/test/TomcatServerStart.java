302f3e50623bbab292ed16b4029ff76ae548fe75
Honghua Zhu <zhh200910@gmail.com>
2023-12-04 14:49:37 +0800

    update TomcatServerStart

--- src/test/java/org/lealone/plugins/tomcat/test/TomcatServerStart.java
+++ src/test/java/org/lealone/plugins/tomcat/test/TomcatServerStart.java
@@ -5,11 +5,33 @@
  */
 package org.lealone.plugins.tomcat.test;
 
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.servlets.DefaultServlet;
+import org.apache.catalina.startup.Tomcat;
 import org.lealone.main.Lealone;
 
 public class TomcatServerStart {
 
     public static void main(String[] args) {
         Lealone.main(args);
+        // startTomcat();
+    }
+
+    // http://localhost:8080/index.html
+    public static void startTomcat() {
+        try {
+            Tomcat tomcat = new Tomcat();
+            tomcat.setPort(8080);
+            tomcat.getConnector();
+            Context ctx = tomcat.addContext("", new File("./src/test/resources/web").getCanonicalPath());
+            Tomcat.addServlet(ctx, "defaultServlet", new DefaultServlet());
+            ctx.addServletMappingDecoded("/", "defaultServlet");
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
