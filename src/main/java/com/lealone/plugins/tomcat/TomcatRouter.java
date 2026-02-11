5bfbc278c7d8a065cc7b9977998cc9de11825f3f
Honghua Zhu <zhh200910@gmail.com>
2026-02-11 23:33:54 +0800

    删降TomcatServiceFilter

--- src/main/java/com/lealone/plugins/tomcat/TomcatRouter.java
+++ src/main/java/com/lealone/plugins/tomcat/TomcatRouter.java
@@ -5,6 +5,7 @@
  */
 package com.lealone.plugins.tomcat;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.apache.catalina.servlets.DefaultServlet;
@@ -15,25 +16,61 @@ import com.lealone.plugins.service.ServiceHandler;
 import com.lealone.plugins.service.http.HttpRouter;
 import com.lealone.plugins.service.http.HttpServer;
 
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class TomcatRouter implements HttpRouter {
+
+    protected TomcatServer tomcatServer;
+
     @Override
     public void init(HttpServer server, Map<String, String> config) {
-        TomcatServer tomcatServer = (TomcatServer) server;
+        tomcatServer = (TomcatServer) server;
+        init(config);
+    }
 
-        tomcatServer.addServlet("serviceServlet", new TomcatServiceServlet(new ServiceHandler(config)));
-        tomcatServer.addServlet("defaultServlet", new DefaultServlet());
+    public void init(Map<String, String> config) {
+        addServlet("serviceServlet", new TomcatServiceServlet(new ServiceHandler(config)), "/service/*");
+        addServlet("defaultServlet", new TomcatDefaultServlet(config), "/*");
+        tomcatServer.getContext().addWelcomeFile("index.html");
+    }
 
-        tomcatServer.addServletMappingDecoded("/service/*", "serviceServlet");
-        tomcatServer.addServletMappingDecoded("/", "defaultServlet");
+    public void addServlet(String name, Servlet servlet, String urlPattern) {
+        tomcatServer.addServlet(name, servlet);
+        tomcatServer.addServletMappingDecoded(urlPattern, name);
+    }
 
+    public void addFilter(String name, HttpFilter filter, String urlPattern) {
         FilterDef def = new FilterDef();
-        def.setFilter(new TomcatServiceFilter(new ServiceHandler(config)));
-        def.setFilterName("serviceFilter");
+        def.setFilter(filter);
+        def.setFilterName(name);
         FilterMap map = new FilterMap();
-        map.setFilterName("serviceFilter");
-        map.addServletName("serviceServlet");
+        map.setFilterName(name);
+        map.addURLPattern(urlPattern);
         tomcatServer.getContext().addFilterDef(def);
         tomcatServer.getContext().addFilterMap(map);
-        tomcatServer.getContext().addWelcomeFile("index.html");
+    }
+
+    private static class TomcatDefaultServlet extends DefaultServlet {
+
+        private final String characterEncoding;
+
+        private TomcatDefaultServlet(Map<String, String> config) {
+            String ce = config.get("character_encoding");
+            if (ce == null)
+                characterEncoding = "UTF-8";
+            else
+                characterEncoding = ce;
+        }
+
+        @Override
+        protected void service(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException {
+            response.setCharacterEncoding(characterEncoding);
+            super.service(request, response);
+        }
     }
 }
